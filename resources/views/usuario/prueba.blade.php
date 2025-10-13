54b86bf4e2b0f1fff64a9cb744ff9be47e999e0c
Santiago Michel <santiagomichel2604@gmail.com>
2025-10-13 17:31:23 -0300

    hago home view

--- resources/views/usuario/prueba.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.layoutUsuario')
-@section('content')
-    <h1>Prueba de Usuario</h1>
-    <p>Contenido específico para usuarios.</p>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('usuario.prueba');
+    return view('usuario.userHome');
 });
+
